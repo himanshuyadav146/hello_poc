f8dfa0c945602a04fd8a673439b5c71b5510843a
Himanshu Yadav <>
2022-08-26 15:49:27 +0530

    Initial commit

---
+++ lib/hello_poc.dart
@@ -0,0 +1,11 @@
+library hello_poc;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+
+  int addTwoNumbers(int v1,int v2){
+    return v1+v2;
+  }
+}
